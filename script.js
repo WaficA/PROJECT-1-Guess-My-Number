7830470caab1b1c8b5a4aaefca6632bec83b69ac
WaficA <wafic.abass@outlook.com>
2022-12-08 23:43:38 +0000

    .

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 "use strict";
+/*
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+document.querySelector(".guess").value = 23; */
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "NO Number!!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Hurray You Guess Right!!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High!!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "YOU LOOSE!!";
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low!!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "YOU LOOSE!!";
+    }
+  }
+});
